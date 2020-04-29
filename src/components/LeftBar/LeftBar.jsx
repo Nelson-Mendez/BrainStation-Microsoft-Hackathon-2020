8af97231f9a7afc7d1dbf6c081d02a4868e53b89
Anna <av.mastoris@gmail.com>
2020-04-28 20:00:45 -0400

    added state to main page and created toggle capabilities

--- src/components/LeftBar/LeftBar.jsx
+++ src/components/LeftBar/LeftBar.jsx
@@ -1,5 +1,48 @@
 import React from "react";
 import "./Leftbar.scss";
-export default function Leftbar(Props) {
-  return <section className="leftbar">asdasdddd</section>;
+
+
+class Leftbar extends React.Component{
+
+  changeHighlight = () => {
+    let item;
+    let other;
+    if (this.props.tasks) {
+      item = document.getElementById('tasks');
+      other = document.getElementById('teams');
+    } else {
+      item = document.getElementById('teams');
+      other = document.getElementById('tasks');
+    }
+    item.classList.add("leftbar__box--highlight");
+    other.classList.remove("leftbar__box--highlight")
+  }
+
+  componentDidMount() {
+    this.changeHighlight()
+  }
+
+  componentDidUpdate(prevProps) {
+    if (prevProps.tasks !== this.props.tasks) {
+      this.changeHighlight()
+    }
+  }
+
+  render() {
+    console.log(this.props.tasks)
+    return (
+      <section className="leftbar">
+        <h3 className="leftbar__title">Productivity</h3>
+        <div id='tasks' className="leftbar__box" onClick={this.props.toggle}>
+          <div className="leftbar__img"></div>
+          <h5 className="leftbar__task">My Tasks</h5>
+        </div>
+        <div id='teams' className="leftbar__box" onClick={this.props.toggle}>
+          <div className="leftbar__img"></div>
+          <h5 className="leftbar__task">My Teams</h5>
+        </div>
+      </section>);
+  }
 }
+
+export default Leftbar;
